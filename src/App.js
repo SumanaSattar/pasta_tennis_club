4ff47237bc68a50c9280c7e2d1c8a70798c1286c
Sumana Sattar <sumana.sattar@coop.se>
2023-12-03 06:02:26 +0100

    testing path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import logo from './images/pasta_logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <div className='Image-wrapper'>
-         <img src={logo} className='Header-image' alt="logo"  />
+        pasta
        </div>
       </header>
       <div className='Hero'>
